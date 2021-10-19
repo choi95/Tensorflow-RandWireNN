c0a1ff781e64f79ca94a7d1467d3933517973aca
choi95 <41821494+choi95@users.noreply.github.com>
2021-10-19 11:54:44 +0900

    Add files via upload

--- model.py
+++ model.py
@@ -28,37 +28,37 @@ class Custom_Model(tf.keras.models.Model):
 
         if self.model_mode is "CIFAR10":
             self.input_layer = tf.keras.layers.InputLayer(input_shape=(32,32,3), name="CIFAR10_input")       
-            self.CIFAR_conv1 = tf.keras.layers.Conv2D(filters, kernel_size=3, strides=(1, 1), padding='same', activation=tf.nn.relu, name="CIFAR10_conv1")        
-            self.CIFAR_conv2 = tf.keras.layers.Conv2D(self.filters, kernel_size=3, strides=(1, 1), padding='same', activation=tf.nn.relu, name="CIFAR10_conv2")                   
+            self.CIFAR_conv1 = tf.keras.layers.Conv2D(32, kernel_size=3, strides=(1, 1), padding='same', activation=tf.nn.relu, name="CIFAR10_conv1")        
+            self.CIFAR_conv2 = tf.keras.layers.Conv2D(64, kernel_size=3, strides=(1, 1), padding='same', activation=tf.nn.relu, name="CIFAR10_conv2")                   
             self.CIFAR_conv3 = RandWire(self.node_num, self.p, self.filters, self.kernel_size, self.graph_mode, self.is_train, name="CIFAR10_conv3")
             self.CIFAR_conv4 = RandWire(self.node_num, self.p, self.filters, self.kernel_size, self.graph_mode, self.is_train, name="CIFAR10_conv4")
-            self.CIFAR_classifier = tf.keras.layers.Conv2D(self.filters, kernel_size=1, strides=(1, 1), padding='same', name="CIFAR10_classifier")       
+            self.CIFAR_classifier = tf.keras.layers.Conv2D(64, kernel_size=1, strides=(1, 1), padding='same', name="CIFAR10_classifier")       
             self.BatchNormalization = tf.keras.layers.BatchNormalization(name="CIFAR10_batch")     
     
         elif self.model_mode is "CIFAR100":
             self.input_layer = tf.keras.layers.InputLayer(input_shape=(32,32,3), name="CIFAR100_input")
             self.CIFAR100_conv1 = tf.keras.layers.Conv2D(filters, kernel_size=3, strides=(1, 1), padding='same', activation=tf.nn.relu, name="CIFAR100_conv1")     
-            self.CIFAR100_conv2 = RandWire(self.node_num, self.p, self.filters, self.kernel_size * 2, self.graph_mode, self.is_train, name="CIFAR100_conv2")           
-            self.CIFAR100_conv3 = RandWire(self.node_num, self.p, self.filters, self.kernel_size * 4, self.graph_mode, self.is_train, name="CIFAR100_conv3")
-            self.CIFAR100_conv4 = RandWire(self.node_num, self.p, self.filters, self.kernel_size * 8, self.graph_mode, self.is_train, name="CIFAR100_conv4")            
+            self.CIFAR100_conv2 = RandWire(self.node_num, self.p, self.filters, self.kernel_size, self.graph_mode, self.is_train, name="CIFAR100_conv2")           
+            self.CIFAR100_conv3 = RandWire(self.node_num, self.p, self.filters, self.kernel_size, self.graph_mode, self.is_train, name="CIFAR100_conv3")
+            self.CIFAR100_conv4 = RandWire(self.node_num, self.p, self.filters, self.kernel_size, self.graph_mode, self.is_train, name="CIFAR100_conv4")            
             self.CIFAR100_classifier = tf.keras.layers.Conv2D(self.filters, kernel_size=1, strides=(1, 1), padding='same', name="CIFAR100_classifier")
     
         elif self.model_mode is "SMALL_REGIME":
             self.input_layer = tf.keras.layers.InputLayer(input_shape=(32,32,3), name="SMALL_input")
-            self.SMALL_conv1 = tf.keras.layers.Conv2D(filters, kernel_size=3, strides=(1, 1), padding='same', activation=tf.nn.relu, name="SMALL_conv1")     
+            self.SMALL_conv1 = tf.keras.layers.Conv2D(self.filters, kernel_size=3, strides=(1, 1), padding='same', activation=tf.nn.relu, name="SMALL_conv1")     
             self.SMALL_conv2 = tf.keras.layers.Conv2D(self.filters, kernel_size=3, strides=(1, 1), padding='same', activation=tf.nn.relu, name="SMALL_conv2")   
             self.SMALL_conv3 = RandWire(self.node_num, self.p, self.filters, self.kernel_size, self.graph_mode, self.is_train, name="SMALL_conv3")
-            self.SMALL_conv4 = RandWire(self.node_num, self.p, self.filters, self.kernel_size * 2, self.graph_mode, self.is_train, name="SMALL_conv4")
-            self.SMALL_conv5 = RandWire(self.node_num, self.p, self.filters, self.kernel_size * 4, self.graph_mode, self.is_train, name="SMALL_conv5")            
+            self.SMALL_conv4 = RandWire(self.node_num, self.p, self.filters, self.kernel_size, self.graph_mode, self.is_train, name="SMALL_conv4")
+            self.SMALL_conv5 = RandWire(self.node_num, self.p, self.filters, self.kernel_size, self.graph_mode, self.is_train, name="SMALL_conv5")            
             self.SMALL_classifier = tf.keras.layers.Conv2D(self.filters, kernel_size=1, strides=(1, 1), padding='same', name="SMALL_classifier")
     
         elif self.model_mode is "REGULAR_REGIME":
             self.input_layer = tf.keras.layers.InputLayer(input_shape=(32,32,3), name="REGULAR_input")
             self.REGULAR_conv1 = tf.keras.layers.Conv2D(filters, kernel_size=3, strides=(1, 1), padding='same', activation=tf.nn.relu, name="REGULAR_conv1")   
             self.REGULAR_conv2 = RandWire(self.node_num // 2, self.p, self.filters, self.kernel_size, self.graph_mode, self.is_train, name="REGULAR_conv2")
-            self.REGULAR_conv3 = RandWire(self.node_num, self.p, self.filters, self.kernel_size * 2, self.graph_mode, self.is_train, name="REGULAR_conv3")           
-            self.REGULAR_conv4 = RandWire(self.node_num, self.p, self.filters, self.kernel_size * 4, self.graph_mode, self.is_train, name="REGULAR_conv4")         
-            self.REGULAR_conv5 = RandWire(self.node_num, self.p, self.filters, self.kernel_size * 8, self.graph_mode, self.is_train, name="REGULAR_conv5")
+            self.REGULAR_conv3 = RandWire(self.node_num, self.p, self.filters, self.kernel_size, self.graph_mode, self.is_train, name="REGULAR_conv3")           
+            self.REGULAR_conv4 = RandWire(self.node_num, self.p, self.filters, self.kernel_size, self.graph_mode, self.is_train, name="REGULAR_conv4")         
+            self.REGULAR_conv5 = RandWire(self.node_num, self.p, self.filters, self.kernel_size, self.graph_mode, self.is_train, name="REGULAR_conv5")
             self.REGULAR_classifier = tf.keras.layers.Conv2D(self.filters, kernel_size=1, strides=(1, 1), padding='same', name="REGULAR_classifier")
     
         self.Dropout = tf.keras.layers.Dropout(self.dropout_rate)
@@ -74,28 +74,34 @@ class Custom_Model(tf.keras.models.Model):
             out = self.CIFAR_conv3(out)         
             out = self.CIFAR_conv4(out)         
             out = self.CIFAR_classifier(out)
-            out = self.BatchNormalization(out)
-            
+            out = self.BatchNormalization(out)          
         elif self.model_mode is "CIFAR100":
             out = self.CIFAR100_conv1(x)
+            out = self.BatchNormalization(out)
             out = self.CIFAR100_conv2(out)
             out = self.CIFAR100_conv3(out)
             out = self.CIFAR100_conv4(out)
             out = self.CIFAR100_classifier(out)
+            out = self.BatchNormalization(out)
         elif self.model_mode is "SMALL_REGIME":
             out = self.SMALL_conv1(x)
+            out = self.BatchNormalization(out)
             out = self.SMALL_conv2(out)
+            out = self.BatchNormalization(out)
             out = self.SMALL_conv3(out)
             out = self.SMALL_conv4(out)
             out = self.SMALL_conv5(out)
             out = self.SMALL_classifier(out)
+            out = self.BatchNormalization(out)
         elif self.model_mode is "REGULAR_REGIME":
             out = self.REGULAR_conv1(x)
+            out = self.BatchNormalization(out)
             out = self.REGULAR_conv2(out)
             out = self.REGULAR_conv3(out)
             out = self.REGULAR_conv4(out)
             out = self.REGULAR_conv5(out)
             out = self.REGULAR_classifier(out)
+            out = self.BatchNormalization(out)
 
         # global average pooling
         batch_size, height, width, channels = tuple(out.shape)
--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 import tensorflow_datasets as tfds
-from tensorflow.python.keras.datasets import cifar10
+from tensorflow.python.keras.datasets import cifar10, cifar100, mnist
 
 def load_data(args):
     if args.dataset_mode is "CIFAR10":
@@ -15,9 +15,6 @@ def load_data(args):
         testDS = tf.data.Dataset.from_tensor_slices(
         (tf.cast(test_images, tf.float32),
          tf.cast(test_labels, tf.int64)))
-        
-        
-        #train_ds, test_ds = tfds.load('cifar10', split=['train','test'], as_supervised=True)
         
         def train_augmentation(image, label):
             image = tf.image.convert_image_dtype(image, tf.float32)
@@ -37,11 +34,21 @@ def load_data(args):
         return trainDS, testDS
 
     elif args.dataset_mode is "CIFAR100":
-        train_ds, test_ds = tfds.load('cifar100', split=['train','test'], as_supervised=True)
+
+        (train_images, train_labels), (test_images, test_labels) = cifar100.load_data()
+        trainDS, testDS = train_images / 255.0, test_images / 255.0
+        
+        trainDS = tf.data.Dataset.from_tensor_slices(
+        (tf.cast(train_images, tf.float32),
+         tf.cast(train_labels, tf.int64)))
+
+        testDS = tf.data.Dataset.from_tensor_slices(
+        (tf.cast(test_images, tf.float32),
+         tf.cast(test_labels, tf.int64)))
         
         def train_augmentation(image, label):
             image = tf.image.convert_image_dtype(image, tf.float32)
-            image = tf.image.random_crop(image, (32,32,50000))
+            image = tf.image.random_crop(image, (32,32,3))
             image = tf.image.random_flip_up_down(image)
             image = tf.image.per_image_standardization(image)
             return image, label
@@ -50,17 +57,28 @@ def load_data(args):
             image = tf.image.convert_image_dtype(image, tf.float32)
             image = tf.image.per_image_standardization(image)
             return image, label
-            
-        train = train_ds.map(train_augmentation).shuffle(100).batch(args.batch_size).repeat()
-        test = test_ds.map(test_augmentation).cache().batch(64)
-        return train, test
+        
+        trainDS = trainDS.map(train_augmentation).shuffle(50000).batch(args.batch_size)
+        testDS = testDS.map(test_augmentation).batch(args.batch_size)
+        
+        return trainDS, testDS
 
     elif args.dataset_mode is "MNIST":
-        train_ds, test_ds = tfds.load('MNIST', split=['train','test'], as_supervised=True)
+
+        (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+        trainDS, testDS = train_images / 255.0, test_images / 255.0
+        
+        trainDS = tf.data.Dataset.from_tensor_slices(
+        (tf.cast(train_images, tf.float32),
+         tf.cast(train_labels, tf.int64)))
+
+        testDS = tf.data.Dataset.from_tensor_slices(
+        (tf.cast(test_images, tf.float32),
+         tf.cast(test_labels, tf.int64)))
         
         def train_augmentation(image, label):
             image = tf.image.convert_image_dtype(image, tf.float32)
-            image = tf.image.random_crop(image, (28,28,50000))
+            image = tf.image.random_crop(image, (28,28,3))
             image = tf.image.random_flip_up_down(image)
             image = tf.image.per_image_standardization(image)
             return image, label
@@ -69,7 +87,8 @@ def load_data(args):
             image = tf.image.convert_image_dtype(image, tf.float32)
             image = tf.image.per_image_standardization(image)
             return image, label
-            
-        train = train_ds.map(train_augmentation).shuffle(100).batch(args.batch_size).repeat()
-        test = test_ds.map(test_augmentation).cache().batch(args.batch_size)
-        return train, test
+        
+        trainDS = trainDS.map(train_augmentation).shuffle(50000).batch(args.batch_size)
+        testDS = testDS.map(test_augmentation).batch(args.batch_size)
+        
+        return trainDS, testDS
